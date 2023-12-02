22630f185145cca3a81144dda888d2b77c2692f9
NguyenMinhKien04 <nguyenminhkien5577@gmail.comm>
2023-12-02 15:45:06 +0700

    new

--- js/main.js
+++ js/main.js
@@ -34,3 +34,45 @@ function show() {
 function momodal(){
     document.getElementById("nenmodal-1").classList.toggle("active");
     }
+
+    /*----------------------------*/
+    document.addEventListener('DOMContentLoaded', function () {
+        const dots = document.querySelectorAll('.dot');
+        const slides = document.querySelectorAll('.slide');
+        let currentIndex = 0;
+     
+        // Function to navigate to a specific slide
+        const goToSlide = (index) => {
+           slides.forEach((slide, i) => {
+              slide.style.marginLeft = `-${index * 850}px`; // Adjust the slide width accordingly
+           });
+     
+           // Update the active dot
+           dots.forEach((dot, i) => {
+              dot.classList.toggle('active', i === index);
+           });
+        };
+     
+        // Click event for each dot
+        dots.forEach((dot, index) => {
+           dot.addEventListener('click', () => {
+              currentIndex = index;
+              goToSlide(currentIndex);
+           });
+        });
+     
+        // Auto-slide functionality (optional)
+        setInterval(() => {
+           currentIndex = (currentIndex + 1) % slides.length;
+           goToSlide(currentIndex);
+        }, 5000); // Adjust the interval as needed
+     });
+    /*-----------------------------------------*/
+    function showCategory(categoryId) {
+        // Hide all categories
+        document.getElementById('category1').style.display = 'none';
+        document.getElementById('category2').style.display = 'none';
+
+        // Show the selected category
+        document.getElementById(categoryId).style.display = 'block';
+    }
